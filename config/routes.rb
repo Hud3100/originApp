970dcc03d0df93e60fddc6420ce8b61c55931555
Hiroaki <hud3100@gmail.com>
2020-03-30 18:50:18 +0900

    companyモデル生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :companies, controllers: {
+    sessions: 'companies/sessions',
+    passwords: 'companies/passwords',
+    registrations: 'companies/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registraions'
+  }
   root 'home#index'
 end
