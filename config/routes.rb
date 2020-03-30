eca7dc0d312977fc1499ed8d20a2f306f95ed362
Hiroaki <hud3100@gmail.com>
2020-03-30 20:27:23 +0900

    company controller名前修正 routes変更 ヘッダーに登録リンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :companies, controllers: {
-    sessions: 'companies/sessions',
-    passwords: 'companies/passwords',
+    sessions:      'companies/sessions',
+    passwords:     'companies/passwords',
     registrations: 'companies/registrations'
   }
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registraions'
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
   }
   root 'home#index'
 end
