ba85e98b2a0bc58a8a89559b0445152453924172
Hiroaki <hud3100@gmail.com>
2020-03-31 19:17:46 +0900

    User Controller生成 プロフィールページの生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  resources :users, only: [:show]
+
+  # ログイン、アカウント編集後、任意のページに遷移させるための記述
+  # devise_for :users, controllers: {
+  #         registrations: 'users/registrations'
+  # }
+
   devise_for :companies, controllers: {
     sessions:      'companies/sessions',
     passwords:     'companies/passwords',
