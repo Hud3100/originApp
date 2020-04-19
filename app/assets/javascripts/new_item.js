e8669335ead4fea871743288aaae31b4a57fd313
Hiroaki <hud3100@gmail.com>
2020-04-19 09:10:54 +0900

    画像複数投稿機能実装完了及び画像が表示されることを確認した

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -1,19 +1,68 @@
-$(function(){
-  //querySelectorでfile_fieldを取得
-  var file_field = document.querySelector('input[type=file]')
-  //fileが選択された時に発火するイベント
-  $('#img-file').change(function(){
-    //選択したfileのオブジェクトをpropで取得
-    var file = $('input[type="file"]').prop('files')[0];
-    //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-    var fileReader = new FileReader();
-    //読み込みが完了すると、srcにfileのURLを格納
-    fileReader.onloadend = function() {
-      var src = fileReader.result
-      var html= `<img src="${src}" width="114" height="80">`
-      //image_box__container要素の前にhtmlを差し込む
-      $('#image-box__container').before(html);
-    }
-    fileReader.readAsDataURL(file);
+$(document).on('turbolinks:load', function(){
+  $(function(){
+   function buildHTML(count) {
+     var html = `<div class="preview-box" id="preview-box__${count}">
+                    <div class="upper-box">
+                      <img src="" alt="preview" width: 50px height: 50px>
+                    </div>
+                    <div class="lower-box">
+                      <div class="update-box">
+                        <label class="edit_btn">編集</label>
+                      </div>
+                      <div class="delete-box" id="delete_btn_${count}">
+                        <span>削除</span>
+                      </div>
+                    </div>
+                  </div>`
+    return html;
+   }
+   function setLabel() {
+     var prevContent = $('.label-content').prev();
+     var labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+     $('.label-content').css('width', labelWidth);
+   }
+
+   $(document).on('change', '.hidden-field', function() {
+
+     setLabel();
+     var id = $(this).attr('id').replace(/[^0-9]/g, '');
+     $('.label-box').attr({id: `label-box--${id}`, for: `micropost_images_attributes_${id}_img`});
+     var file = this.files[0];
+     var reader = new FileReader();
+     reader.readAsDataURL(file);
+     reader.onload = function() {
+       var image = this.result;
+       if ($(`#preview-box__${id}`).length == 0) {
+         var count = $('.preview-box').length;
+         var html = buildHTML(id);
+         var prevContent = $('.label-content').prev();
+         $(prevContent).append(html);
+       }
+       $(`#preview-box__${id} img`).attr('src', `${image}`);
+       var count = $('.preview-box').length;
+       if (count == 5) {
+         $('.label-content').hide();
+       }
+       setLabel();
+       if(count < 5) {
+         $('.label-box').attr({id: `label-box--${count}`, for: `micropost_images_attributes_${count}_img`});
+       }
+     }
+   });
+   $(document).on('click', '.delete-box', function() {
+     var count = $('.preview-box').length;
+     setLabel(count);
+     var id = $(this).attr('id').replace(/[^0-9]/g, '');
+     $(`#preview-box__${id}`).remove();
+     console.log('new');
+     var count = $('.preview-box').length;
+     if(count == 4) {
+       $('.label-content').show();
+     }
+     setLabel(count);
+     if(id < 5){
+       $('.label-box').attr({id: `label-box--${id}`, for: `micropost_images_attributes_${id}_img`});
+     }
+   });
   });
 });
