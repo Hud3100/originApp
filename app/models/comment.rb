c05fa3911a42f510268d94606bbc7a652156d700
Hiroaki <hud3100@gmail.com>
2020-05-11 23:48:41 +0900

    companyモデル　コメントモデルにカラムを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :company
   belongs_to :micropost,optional: true
+  validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_103816) do
+ActiveRecord::Schema.define(version: 2020_05_11_134350) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_103816) do
     t.bigint "micropost_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["company_id"], name: "index_comments_on_company_id"
     t.index ["micropost_id"], name: "index_comments_on_micropost_id"
   end
@@ -30,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_103816) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_companies_on_email", unique: true
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
   end
