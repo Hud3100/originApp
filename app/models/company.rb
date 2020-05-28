6b0ddf2c261d7afbac3034ef1fd4b94baf002c40
Hiroaki <hud3100@gmail.com>
2020-05-28 23:41:34 +0900

    Company のmodel specを追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,6 @@
 class Company < ApplicationRecord
+  validates :name, presence: true
   has_many :comments, as: :commentable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :notifications, as: :notificationable
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitorable_id', dependent: :destroy, as: :visitorable
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visitedable_id', dependent: :destroy, as: :visitedable
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe Company, type: :model do
   context "バリデーション: " do
-    it "企業名およびメールアドレスを有するカンパニーは有効" do
+    it "企業名、メールアドレス、パスワードを有するカンパニーは有効" do
       company = Company.new(
         email: "samplecompany@company.com",
         name: "Sample Company",
@@ -11,12 +11,28 @@ RSpec.describe Company, type: :model do
     end
 
     it "パスワードがないカンパニーは無効" do
+      company = Company.new(
+        email: "samplecompany@company.com",
+        email: "samplecompany@company.com",
+        name: "Sample Company",
+      )
+      expect(company).not_to be_valid
     end
 
     it "企業名がないカンパニーは無効" do
+      company = Company.new(
+        email: "samplecompany@company.com",
+        password: "password"
+      )
+      expect(company).not_to be_valid
     end
 
     it "メールアドレスがないカンパニーは無効" do
+      company = Company.new(
+        name: "Sample Company",
+        password: "password"
+      )
+      expect(company).not_to be_valid
     end
   end
 end
