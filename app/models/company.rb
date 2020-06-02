cdaedd18c312278daaaa14a83807cd2e69ce6be5
Hiroaki <hud3100@gmail.com>
2020-06-02 22:44:58 +0900

    micropost model にバリデーションを追加 micropost model と comment model のリレーションを変更 user model に emailのバリデーションを追加 micropost の model specを追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
-  validates :name, presence: true
-  has_many :comments, as: :commentable
+  validates :name, :email, uniqueness: { case_sensitive: :false}, presence: true
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :notifications, as: :notificationable
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitorable_id', dependent: :destroy, as: :visitorable
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visitedable_id', dependent: :destroy, as: :visitedable
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 RSpec.describe Company, type: :model do
+  let(:company) { create(:company) }
+  let(:micropost) { create(:micropost) }
   context "バリデーション: " do
     it "企業名、メールアドレス、パスワードを有するカンパニーは有効" do
       company = Company.new(
@@ -33,6 +35,65 @@ RSpec.describe Company, type: :model do
         password: "password"
       )
       expect(company).not_to be_valid
+    end
+
+    it "同じ企業名のカンパニーの複数登録不可" do
+      company = Company.create(
+        name: "Sample Company",
+        email: "company@company.com",
+        password: "password"
+      )
+      another_company = Company.create(
+        name: "Sample Company",
+        email: "another_company@company.com",
+        password: "password"
+      )
+      expect(another_company).not_to be_valid
+      expect(another_company.errors[:name]).to include("はすでに存在します")
+    end
+
+    it "同じメールアドレスのカンパニーの複数登録は不可" do
+      company = Company.create(
+        name: "Sample Company",
+        email: "company@company.com",
+        password: "password"
+      )
+      another_company = Company.create(
+        name: "Another Company",
+        email: "company@company.com",
+        password: "password"
+      )
+      expect(another_company).not_to be_valid
+      expect(another_company.errors[:email]).to include("はすでに存在します")
+    end
+  end
+
+  context "アソシエーション:" do
+    it "Commentモデルをもつ" do
+      t = Company.reflect_on_association(:comments)
+      expect(t.macro).to eq(:has_many)
+      expect(t.class_name).to eq "Comment"
+    end
+
+    it "カンパニーを削除すると、カンパニーがもつCommentも削除される" do
+      comment = company.comments.create(
+        title: "Sample",
+        content: "Company comments",
+        micropost_id: micropost.id
+      )
+      expect(comment).to be_valid
+      expect { company.destroy }.to change{ Comment.count }.by(-1)
+    end
+  end
+
+  context "削除:" do
+    it "カンパニーは削除できる" do
+      company = Company.create(
+        name: "Sample company",
+        email: "samplecompany@company.com",
+        password: "password"
+      )
+      expect { company.destroy }.to change{ Company.count }.by(-1)
     end
   end
 end
