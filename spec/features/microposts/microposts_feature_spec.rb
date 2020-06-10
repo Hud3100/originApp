f9cd1919d9508e1a70296dfbdba43ab376534d94
Hiroaki <hud3100@gmail.com>
2020-06-10 18:38:48 +0900

    Rspecのヘルパーメソッド（カンパニー用）を追加 Feature specの枠組みを作成

--- spec/features/microposts/microposts_feature_spec.rb
+++ spec/features/microposts/microposts_feature_spec.rb
@@ -1,54 +1,69 @@
 require 'rails_helper'
 
 RSpec.feature '質問の投稿とコメント機能' do
-  let(:testuser) { create:user,
+  given(:testuser) { create:user,
                    name: "Test User",
                    email: "testuser@user.com",
                   password: "sampleuser",
                   password_confirmation: "sampleuser"}
-  let(:anotheruser) { create :user }
-  let(:company) { create :company }
+  given(:anotheruser) { create :user }
+  given(:company) { create :company }
   background do
     # User.create(name: "Sample User", email: "samplecompany@company.com", password: "samplepassword")
     # Company.create!(name: "Sample Company", email: "samplecompany@company.com", password: "samplepassword")
   end
 
-  scenario '質問の投稿' do
-    login(testuser)
-    visit new_micropost_path
-    fill_in 'form-title', with: "オデッセイのカスタムについて"
-    fill_in 'form-content', with: "オデッセイの乗り心地改善やマフラーを変更したいです。"
-    fill_in '予算', with: "30"
-    fill_in 'form-search-car-name', with: "オデッセイ"
-    check '内装'
-    attach_file "micropost[images_attributes][1][img]", "spec/fixtures/test.jpg"
-    expect { click_on '投稿する' }.to change { Micropost.count }.by(1)
-    expect(page).to have_content "投稿されました"
+  context '質問の投稿画面' do
+    scenario 'ユーザーが質問を投稿する' do
+      login(testuser)
+      visit new_micropost_path
+      fill_in 'form-title', with: "オデッセイのカスタムについて"
+      fill_in 'form-content', with: "オデッセイの乗り心地改善やマフラーを変更したいです。"
+      fill_in '予算', with: "30"
+      fill_in 'form-search-car-name', with: "オデッセイ"
+      check '内装'
+      attach_file "micropost[images_attributes][1][img]", "spec/fixtures/test.jpg"
+      expect { click_on '投稿する' }.to change { Micropost.count }.by(1)
+      expect(page).to have_content "投稿されました"
+    end
+
+    scenario '質問に件名がなければエラーメッセージが表示される' do
+      skip
+    end
+
+    scenario '質問に内容がなければエラーメッセージが表示される' do
+      skip
+    end
   end
 
-  # scenario '質問のコメントをカンパニーが行う' do
-  #   login_as(company)
-  #   visit micropost_path(Micropost.first)
-  #   fill_in '件名', with: "カスタムのご提案"
-  #   fill_in '内容', with: "乗り心地改善に関しては、サスペンションやショックアブソーバーの交換を
-  #   おすすめします。マフラーは当社自慢の可変バルブ付きマフラーがございます。"
-  #   click_on 'コメントする'
-  #   expect(page).to have_content "コメントが投稿されました。"
-  #   # 画像の添付
-  # end
+  context 'コメント' do
+    scenario 'ユーザーが質問に投稿し、カスタマーショップや他ユーザーがコメントを投稿する' do
+    #   login_as(company)
+    #   visit micropost_path(Micropost.first)
+    #   fill_in '件名', with: "カスタムのご提案"
+    #   fill_in '内容', with: "乗り心地改善に関しては、サスペンションやショックアブソーバーの交換を
+    #   おすすめします。マフラーは当社自慢の可変バルブ付きマフラーがございます。"
+    #   click_on 'コメントする'
+    #   expect(page).to have_content "コメントが投稿されました。"
+    #   # 画像の添付
+    end
 
-  # scenario 'コメントorタイトルの内容が入力されていなければ弾く'
+    scenario 'コメントにタイトルが入力されていなければエラーメッセージを表示する' do
+      skip
+    end
 
-  # scenario '質問のコメントをユーザーが行う' do
-  #   skip
-  #   # 画像の添付
-  # end
+    scenario 'コメントに内容が入力されていなければエラーメッセージを表示する' do
+      skip
+    end
+  end
 
-  # scenario '質問をユーザーがお気に入り登録する' do
-  #   skip
-  # end
+  context 'お気に入り' do
+    scenario '質問に他ユーザーがお気に入り登録する' do
+      skip
+    end
 
-  # scenario '質問をユーザーがお気に入りから解除する' do
-  #   skip
-  # end
+    scenario '質問をユーザーがお気に入りから解除する' do
+      skip
+    end
+  end
 end
--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -12,6 +12,19 @@ module LoginMacros
     expect(page).to have_content 'ログアウトしました。'
   end
 
+  def login_as_company(company)
+    visit new_company_session_path
+    fill_in 'email', with: company.email
+    fill_in 'company_password', with: "samplecompany"
+    find('.btn-login').click
+    expect(page).to have_content
+  end
+
+  def logout_as_company
+    click_link 'ログアウト'
+    expect(page).to have_content 'ログアウトしました。'
+  end
+
   def act_as(user)
     login user
     yield
