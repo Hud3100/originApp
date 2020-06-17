692b599ebf762ebe553addb5a411f1c5f39fe654
Hiroaki <hud3100@gmail.com>
2020-06-17 17:38:39 +0900

    Notification ControllerのRequest Specを追加

---
+++ spec/requests/notifications_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "NotificationsController", type: :request do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+  describe "GET #index" do
+    it 'リクエストが成功すること' do
+      get notifications_url
+      expect(response.status).to eq 200
+    end
+
+    it '通知状態が表示されていること' do
+      get notifications_url
+      expect(response.body).to include "通知はありません"
+    end
+  end
+end
