51846bb5096fe05392160f1abd6fd310e8fdf365
Hiroaki <hud3100@gmail.com>
2020-06-25 21:26:51 +0900

    ユーザーのプロフィール画面のデザイン修正 投稿内容のパーシャルの描画デザイン修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :user_signed_in?, only: :show
   def show
     @user = User.find(params[:id])
+    @microposts = Micropost.find_by(user_id: @user.id)
     @favorite_microposts = @user.favorite_microposts
   end
 end
