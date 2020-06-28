88abd66024a93e4e29d108ff80b48d6f08b5fc7a
Hiroaki <hud3100@gmail.com>
2020-06-28 21:32:47 +0900

    コメントのViewをパーシャル化 カスタムショップのマイページのデザインを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,6 @@ end
 class Company::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    permit(:sign_up, keys: [:profile])
+    permit(:sign_up, keys: [:name, :profile])
   end
 end
