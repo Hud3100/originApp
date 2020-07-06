1a2a9af0abbedb3a6150e62af833d64e8693346b
Hiroaki <hud3100@gmail.com>
2020-07-06 18:35:46 +0900

    Jqueryのファイル読み込みのバグをデバッグ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
+//= require jquery
 //= require turbolinks
 //= require rails-ujs
 //= require jquery3
+//= require activestorage
 //= require popper
 //= require bootstrap-sprockets
 //= require jquery-ui/widgets/autocomplete
+//= require_tree .
